e2359b2142aa95b39b5b652b9ecba5bda23b4e70
hisao5232 <hisao52321983@gmail.com>
2026-02-20 15:28:26 +0900

    fix next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   reactCompiler: true,
   images: {
-    unoptimized: true, // 静的書き出し時は画像の最適化をオフにする必要があります
+    unoptimized: true,
   },
 };
 
--- frontend/src/app/items/keyword/[keyword]/page.tsx
+++ frontend/src/app/items/keyword/[keyword]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const runtime = "edge";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
